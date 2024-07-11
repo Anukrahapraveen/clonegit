e6f7c90645c26f018eb15b3893011a252a625de1
Anukrahapraveen <pranukumar2310@gmail.com>
2024-07-11 16:49:34 +0530

    this is my first project

---
+++ src/clo/org/anotherclo.java
@@ -0,0 +1,16 @@
+package clo.org;
+
+import com.sun.tools.javac.launcher.Main;
+
+public class anotherclo {
+  private void anu() {
+	System.out.println("start cloning.........");
+	}
+  
+  public static void main(String[] args) {
+	 anotherclo o = new anotherclo();
+	 o.anu();
+}
+	
+	
+}
